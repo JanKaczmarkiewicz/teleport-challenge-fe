12f674c37e5154adf8dec285c074014a9fbe1eeb
Jan Kaczmarkiewicz <positiveKiddo@gmail.com>
2022-03-22 10:22:14 +0100

    test: sorting

--- frontend/src/components/App/tests/FolderRoute.test.tsx
+++ frontend/src/components/App/tests/FolderRoute.test.tsx
@@ -1,5 +1,5 @@
 import userEvent from '@testing-library/user-event';
-import { screen } from '@testing-library/react';
+import { screen, within } from '@testing-library/react';
 import { generateFolderPath } from '../../../routes';
 import setup from './setup';
 
@@ -24,4 +24,47 @@ test('should display formatted size', async () => {
     expect(screen.getByText('500 B')).toBeInTheDocument();
     expect(screen.getByText('320 kB')).toBeInTheDocument();
     expect(screen.getByText('3.32 MB')).toBeInTheDocument();
+});
+
+test('should sort by name ascending', async () => {
+    setup({ at: generateFolderPath() });
+
+    const [first, second] = screen.getAllByRole('listitem');
+
+    expect(within(first).getByText('css')).toBeInTheDocument();
+    expect(within(second).getByText('favorites')).toBeInTheDocument();
+});
+
+test('should sort by name descending', async () => {
+    setup({ at: generateFolderPath() });
+
+    userEvent.click(screen.getByText('Name'));
+
+    const [first, second] = screen.getAllByRole('listitem');
+
+    expect(within(first).getByText('nested')).toBeInTheDocument();
+    expect(within(second).getByText('music')).toBeInTheDocument();
+});
+
+test('should sort by size ascending', async () => {
+    setup({ at: generateFolderPath('nested', 'foo', 'bar') });
+
+    userEvent.click(screen.getByText('Size'));
+
+    const [first, second] = screen.getAllByRole('listitem');
+
+    expect(within(first).getByText('500 B')).toBeInTheDocument();
+    expect(within(second).getByText('320 kB')).toBeInTheDocument();
+});
+
+test('should order by size descending', async () => {
+    setup({ at: generateFolderPath('nested', 'foo', 'bar') });
+
+    userEvent.click(screen.getByText('Size'));
+    userEvent.click(screen.getByText('Size'));
+
+    const [first, second] = screen.getAllByRole('listitem');
+
+    expect(within(first).getByText('3.32 MB')).toBeInTheDocument();
+    expect(within(second).getByText('320 kB')).toBeInTheDocument();
 });

b743c189b0aebd65517270ef10e2706413f2fb75
Jan Kaczmarkiewicz <positiveKiddo@gmail.com>
2022-03-23 09:35:40 +0100

    feat: add folder response structure

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,11 +1,32 @@
-use rocket::routes;
-
 #[macro_use]
 extern crate rocket;
 
-#[get("/")]
-fn folder() -> &'static str {
-    "Hello world"
+use rocket::{
+    http::Status,
+    routes,
+    serde::{json::Json, Serialize},
+};
+
+#[derive(Debug, Serialize)]
+#[serde(rename_all = "camelCase")]
+struct FolderItem {
+    size_kb: u32,
+    name: &'static str,
+    r#type: &'static str,
+}
+
+#[derive(Debug, Serialize)]
+struct FolderResponse {
+    name: &'static str,
+    items: Vec<FolderItem>,
+}
+
+#[get("/folder")]
+fn folder() -> Result<Json<FolderResponse>, Status> {
+    Ok(Json(FolderResponse {
+        items: vec![],
+        name: "root",
+    }))
 }
 
 #[launch]

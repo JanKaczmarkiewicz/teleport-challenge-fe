48fb87d98123bfc23c7e499b61a9ac51572dcc09
Jan Kaczmarkiewicz <positiveKiddo@gmail.com>
2022-03-24 13:43:19 +0100

    feat: add auth based routing

--- frontend/src/components/App/App.tsx
+++ frontend/src/components/App/App.tsx
@@ -4,28 +4,41 @@ import colors from '../../styleTokens/colors';
 import routes, { generateFolderPath } from '../../routes';
 import FolderPage from '../FolderPage/FolderPage';
 import NotFoundPage from '../NotFoundPage/NotFoundPage';
-import PrivateRoute from '../PrivateRoute/PrivateRoute';
+import AuthenticationProvider from '../AuthenticationProvider/AuthenticationProvider';
+import LoggedOnlyRoute from '../AuthenticationProvider/parts/LoggedOnlyRoute';
+import LoginPage from '../LoginPage/LoginPage';
+import NotLoggedOnlyRoute from '../AuthenticationProvider/parts/NotLoggedOnlyRoute';
 
 const iconContextConfig = { color: colors.boulder };
 
 const App = () => (
-    <IconContext.Provider value={iconContextConfig}>
-        <Routes>
-            <Route
-                element={<Navigate to={generateFolderPath()} />}
-                path={routes.root}
-            />
-            <Route
-                element={
-                    <PrivateRoute>
-                        <FolderPage />
-                    </PrivateRoute>
-                }
-                path={routes.folder}
-            />
-            <Route element={<NotFoundPage />} path={routes.any} />
-        </Routes>
-    </IconContext.Provider>
+    <AuthenticationProvider>
+        <IconContext.Provider value={iconContextConfig}>
+            <Routes>
+                <Route
+                    element={<Navigate to={generateFolderPath()} />}
+                    path={routes.root}
+                />
+                <Route
+                    element={
+                        <LoggedOnlyRoute>
+                            <FolderPage />
+                        </LoggedOnlyRoute>
+                    }
+                    path={routes.folder}
+                />
+                <Route
+                    element={
+                        <NotLoggedOnlyRoute>
+                            <LoginPage />
+                        </NotLoggedOnlyRoute>
+                    }
+                    path={routes.login}
+                />
+                <Route element={<NotFoundPage />} path={routes.any} />
+            </Routes>
+        </IconContext.Provider>
+    </AuthenticationProvider>
 );
 
 export default App;

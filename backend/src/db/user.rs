81f9aa142c538214ec3197aa9c69d5013655802c
Jan Kaczmarkiewicz <positiveKiddo@gmail.com>
2022-03-25 10:44:10 +0100

    feat: use argon2 for password hashing

--- backend/src/db/user.rs
+++ backend/src/db/user.rs
@@ -1,3 +1,5 @@
+use argon2;
+
 pub struct User {
     pub id: &'static str,
     pub username: &'static str,
@@ -7,28 +9,28 @@ pub struct User {
 const USERS: [User; 2] = [
     User {
         id: "1",
-        password: "TODO",
-        username: "andy",
+        password: "$argon2i$v=19$m=4096,t=3,p=1$cmFuZG9tc2FsdDI1$crFi9zLftzxAri3geuHvdWhbVwZZppSvdZW+FEpStAw", // weirdcloud46
+        username: "Patricia12",
     },
     User {
         id: "2",
-        password: "TODO",
-        username: "andy",
+        password:"$argon2i$v=19$m=4096,t=3,p=1$cmFuZG9tc2FsdDU0$Kqy+jKgqI05K/n+wtaQYQFb2vNovWb2+qCyuxRAxj9s", // calmsnake34
+        username: "Robert74",
     },
 ];
 
 pub fn get_user_by_credentials(username: &str, password: &str) -> Option<User> {
     USERS
         .into_iter()
-        .find(|user| user.username == username)
+        .find(|user| username.eq(user.username))
         .into_iter()
-        .find(|user| user.password == hash_password(password))
+        .find(|user| compare_passwords(user.password, password))
 }
 
 pub fn get_user_by_id(id: &str) -> Option<User> {
     USERS.into_iter().find(|user| user.id == id)
 }
 
-fn hash_password(password: &str) -> String {
-    String::from(password)
+pub fn compare_passwords(hashed: &str, password: &str) -> bool {
+    argon2::verify_encoded(hashed, password.as_bytes()).unwrap_or_default()
 }

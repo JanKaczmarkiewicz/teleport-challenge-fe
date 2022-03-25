e62b2c4fb5ee56133325d9fc5b50441d5eca6027
Jan Kaczmarkiewicz <positiveKiddo@gmail.com>
2022-03-25 16:49:03 +0100

    test: update tests after adding async

--- frontend/src/components/App/tests/FolderRoute.test.tsx
+++ frontend/src/components/App/tests/FolderRoute.test.tsx
@@ -1,70 +1,77 @@
 import userEvent from '@testing-library/user-event';
 import { screen, within } from '@testing-library/react';
 import { generateFolderPath } from '../../../routes';
-import setup from './setup';
+import baseSetup, { login } from './setup';
+
+const setup = async () => {
+    baseSetup({ at: generateFolderPath() });
+    login();
+    expect(await screen.findByText('My folder')).toBeVisible();
+};
 
 test('should navigate between folders', async () => {
-    setup({ at: generateFolderPath() });
+    await setup();
 
-    // click on folder
     userEvent.click(screen.getByText('nested'));
 
-    // expect to be redirected to folder
-    expect(screen.getByText('foo')).toBeInTheDocument();
+    expect(await screen.findByText('fileInNestedFolder.rs')).toBeVisible();
 
-    // click on breadcrumb to return
     userEvent.click(screen.getByText('My folder'));
 
-    expect(screen.getByText('nested')).toBeInTheDocument();
+    expect(await screen.findByText('nested')).toBeVisible();
 });
 
 test('should display formatted size', async () => {
-    setup({ at: generateFolderPath('nested', 'foo', 'bar') });
+    await setup();
 
-    expect(screen.getByText('500 B')).toBeInTheDocument();
-    expect(screen.getByText('320 kB')).toBeInTheDocument();
-    expect(screen.getByText('3.32 MB')).toBeInTheDocument();
+    expect(screen.getByText('500 B')).toBeVisible();
+    expect(screen.getByText('320 kB')).toBeVisible();
+    expect(screen.getByText('3.2 MB')).toBeVisible();
 });
 
 test('should sort by name ascending', async () => {
-    setup({ at: generateFolderPath() });
+    await setup();
 
-    const [first, second] = screen.getAllByRole('listitem');
+    const [first, second, third] = screen.getAllByRole('listitem');
 
-    expect(within(first).getByText('css')).toBeInTheDocument();
-    expect(within(second).getByText('favorites')).toBeInTheDocument();
+    expect(within(first).getByText('nested')).toBeVisible();
+    expect(within(second).getByText('large.rs')).toBeVisible();
+    expect(within(third).getByText('medium.rs')).toBeVisible();
 });
 
 test('should sort by name descending', async () => {
-    setup({ at: generateFolderPath() });
+    await setup();
 
-    userEvent.click(screen.getByText('Name'));
+    userEvent.click(screen.getByRole('button', { name: 'Name' }));
 
-    const [first, second] = screen.getAllByRole('listitem');
+    const [first, second, third] = screen.getAllByRole('listitem');
 
-    expect(within(first).getByText('nested')).toBeInTheDocument();
-    expect(within(second).getByText('music')).toBeInTheDocument();
+    expect(within(first).getByText('nested')).toBeVisible();
+    expect(within(second).getByText('small.rs')).toBeVisible();
+    expect(within(third).getByText('medium.rs')).toBeVisible();
 });
 
 test('should sort by size ascending', async () => {
-    setup({ at: generateFolderPath('nested', 'foo', 'bar') });
+    await setup();
 
-    userEvent.click(screen.getByText('Size'));
+    userEvent.click(screen.getByRole('button', { name: 'Size' }));
 
-    const [first, second] = screen.getAllByRole('listitem');
+    const [first, second, third] = screen.getAllByRole('listitem');
 
-    expect(within(first).getByText('500 B')).toBeInTheDocument();
-    expect(within(second).getByText('320 kB')).toBeInTheDocument();
+    expect(within(first).getByText('-')).toBeVisible();
+    expect(within(second).getByText('500 B')).toBeVisible();
+    expect(within(third).getByText('320 kB')).toBeVisible();
 });
 
 test('should order by size descending', async () => {
-    setup({ at: generateFolderPath('nested', 'foo', 'bar') });
+    await setup();
 
-    userEvent.click(screen.getByText('Size'));
-    userEvent.click(screen.getByText('Size'));
+    userEvent.click(screen.getByRole('button', { name: 'Size' }));
+    userEvent.click(screen.getByRole('button', { name: 'Size' }));
 
-    const [first, second] = screen.getAllByRole('listitem');
+    const [first, second, third] = screen.getAllByRole('listitem');
 
-    expect(within(first).getByText('3.32 MB')).toBeInTheDocument();
-    expect(within(second).getByText('320 kB')).toBeInTheDocument();
+    expect(within(first).getByText('-')).toBeVisible();
+    expect(within(second).getByText('3.2 MB')).toBeVisible();
+    expect(within(third).getByText('320 kB')).toBeVisible();
 });

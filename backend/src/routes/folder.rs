c2faa8a95915179cfade871bee1f537b0ebd1ce2
Jan Kaczmarkiewicz <positiveKiddo@gmail.com>
2022-03-27 11:00:50 +0200

    docs: add PathBuf safety explanation

--- backend/src/routes/folder.rs
+++ backend/src/routes/folder.rs
@@ -5,6 +5,7 @@ use crate::{
 use rocket::{get, http::Status, serde::json::Json};
 use std::path::PathBuf;
 
+// SAFETY: https://github.dev/SergioBenitez/Rocket/blob/e382bc584b478092fdd53eefb9f90ea9c08a6967/core/lib/src/request/from_param.rs#L319-L320
 #[get("/<path..>")]
 pub fn folder_data(path: PathBuf, _auth: Auth) -> Result<Json<DirectoryData>, Status> {
     let found_folder = get_folder(path);
